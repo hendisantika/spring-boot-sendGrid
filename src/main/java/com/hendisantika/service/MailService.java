b3df81f19392f06a806b33b9085e221f72de98b9
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-19 08:11:11 +0700

    #4 - Add sendTextEmail in MailService

--- src/main/java/com/hendisantika/service/MailService.java
+++ src/main/java/com/hendisantika/service/MailService.java
@@ -1,7 +1,16 @@
 package com.hendisantika.service;
 
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,5 +25,29 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 public class MailService {
+    public String sendTextEmail() throws IOException {
+        /*
+         * The sender email should be the same as we used to Create a Single Sender
+         * Verification
+         */
+        Email from = new Email("hendisantika@yahoo.co.id");
+        String subject = "The subject";
+        Email to = new Email("hendi@yopmail.com");
+        Content content = new Content("text/plain", "This is a test email");
+        Mail mail = new Mail(from, subject, to, content);
+
+        SendGrid sg = new SendGrid("SENDGRID_API_KEY");
+        Request request = new Request();
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            Response response = sg.api(request);
+            log.info(response.getBody());
+            return response.getBody();
+        } catch (IOException ex) {
+            throw ex;
+        }
+    }
 
 }
