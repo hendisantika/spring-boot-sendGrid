56a517630b4f3d8e4696cce617768960fc2b8e00
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-19 08:09:57 +0700

    #3 - Add MailService

---
+++ src/main/java/com/hendisantika/service/MailService.java
@@ -0,0 +1,20 @@
+package com.hendisantika.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-sendGrid
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 19/02/22
+ * Time: 08.09
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@Slf4j
+public class MailService {
+
+}
