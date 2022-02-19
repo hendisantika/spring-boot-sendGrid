5c895363aeb140fc59186254239b98c68c22b157
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-19 08:17:13 +0700

    #6 - Add DynamicTemplatePersonalization in MailService

--- src/main/java/com/hendisantika/service/MailService.java
+++ src/main/java/com/hendisantika/service/MailService.java
@@ -1,5 +1,6 @@
 package com.hendisantika.service;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.sendgrid.Method;
 import com.sendgrid.Request;
 import com.sendgrid.Response;
@@ -7,10 +8,14 @@ import com.sendgrid.SendGrid;
 import com.sendgrid.helpers.mail.Mail;
 import com.sendgrid.helpers.mail.objects.Content;
 import com.sendgrid.helpers.mail.objects.Email;
+import com.sendgrid.helpers.mail.objects.Personalization;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -83,5 +88,29 @@ public class MailService {
         }
     }
 
+    // This class handle the dynamic data for the template
+    // Feel free to customize this class
+    private static class DynamicTemplatePersonalization extends Personalization {
 
+        @JsonProperty(value = "dynamic_template_data")
+        private Map<String, String> dynamic_template_data;
+
+        @JsonProperty("dynamic_template_data")
+        public Map<String, String> getDynamicTemplateData() {
+            if (dynamic_template_data == null) {
+                return Collections.emptyMap();
+            }
+            return dynamic_template_data;
+        }
+
+        public void addDynamicTemplateData(String key, String value) {
+            if (dynamic_template_data == null) {
+                dynamic_template_data = new HashMap<String, String>();
+                dynamic_template_data.put(key, value);
+            } else {
+                dynamic_template_data.put(key, value);
+            }
+        }
+
+    }
 }
